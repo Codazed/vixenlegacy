1e379e9fb841f953b397ecbc9c12caf79135750b
Joshua Luce <joshuabluce@protonmail.com>
2020-04-16 10:49:11 -0500

    Base queue command working

---
+++ src/commands/music/queue.js
@@ -0,0 +1,92 @@
+const commando = require('discord.js-commando');
+const dur = require('format-duration');
+let botclient;
+
+module.exports = class QueueCommand extends commando.Command {
+    constructor(client) {
+        super(client, {
+            name: 'queue',
+            aliases: ['queue'],
+            group: 'music',
+            memberName: 'queue',
+            description: 'Queue management',
+            details: `Queue management commands.`
+        });
+        botclient = client;
+    }
+
+    async run(msg, args) {
+        let controller = botclient.vixen.audioController;
+
+        if (args.length < 1) {
+            // Print queue to Discord
+            let {table} = require('table');
+            let queue = controller.getQueue(msg.guild.id).slice();
+            let messageString;
+            let data = [['Pos', 'Title', 'Duration', 'Requester', 'ETA']];
+            let timeTil = controller.getTimeTilNext(msg.guild.id);
+            let tableCfg = {
+                border: {
+                    topBody: ``,
+                    topJoin: ``,
+                    topLeft: ``,
+                    topRight: ``,
+
+                    bottomBody: ``,
+                    bottomJoin: ``,
+                    bottomLeft: ``,
+                    bottomRight: ``,
+
+                    bodyLeft: ``,
+                    bodyRight: ``,
+                    bodyJoin: `|`,
+
+                    joinBody: `─`,
+                    joinLeft: ``,
+                    joinRight: ``,
+                    joinJoin: `┼`
+                },
+                columns: {
+                    0: {
+                        alignment: 'left'
+                    },
+                    1: {
+                        alignment: 'left'
+                    },
+                    2: {
+                        alignment: 'center'
+                    },
+                    3: {
+                        alignment: 'center'
+                    },
+                    4: {
+                        alignment: 'center'
+                    }
+                },
+                drawHorizontalLine: (index) => {
+                    return index === 1;
+                }
+            }
+            let page = 1;
+            let index = 1;
+            queue.forEach(video => {
+                if (video.requester.nickname !== null && video.requester.nickname !== undefined) {
+                    data.push([index, video.title, dur(video.duration*1000), video.requester.nickname, dur(timeTil)]);
+                } else {
+                    data.push([index, video.title, dur(video.duration*1000), video.requester.user.username, dur(timeTil)]);
+                }
+                index++;
+                if (`Queue page ${page}\`\`\`${table(data, tableCfg)}\`\`\``.length >= 2000) {
+                    let overflow = data.pop();
+                    msg.channel.send(`Queue page ${page}\`\`\`${table(data, tableCfg)}\`\`\``);
+                    page++;
+                    data = [['Pos', 'Title', 'Duration', 'Requester', 'ETA'], overflow];
+                }
+                timeTil += video.duration*1000;
+            });
+            messageString = table(data, tableCfg);
+            await msg.channel.send(`Queue page ${page}\`\`\`${messageString}\`\`\``);
+            await msg.channel.send(`Summary: ${index-1} items in queue. Queue finished in ${dur(timeTil)}`);
+        }
+    }
+}
