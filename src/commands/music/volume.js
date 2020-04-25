32335334300e66462a2e3dacb6a5a58a843564db
Joshua Luce <joshuabluce@protonmail.com>
2020-04-24 22:48:51 -0500

    Added maxVolume to volume command

--- src/commands/music/volume.js
+++ src/commands/music/volume.js
@@ -14,11 +14,17 @@ module.exports = class VolumeCommand extends commando.Command {
 
     async run(msg, args) {
         let controller = this.client.vixen.audioController;
+        let maxVolume = 150;
         let volargs = args.split(" ");
         if (volargs[0]) {
-            let properValue = volargs[0]*0.01;
+            let properValue;
+            if (volargs[0] > maxVolume) {
+                properValue = maxVolume*0.01;
+            } else {
+                properValue = volargs[0]*0.01;
+            }
             controller.setVolume(msg.guild.id, properValue);
-            msg.channel.send(`Set the volume to ${volargs[0]}%`);
+            msg.channel.send(`Set the volume to ${properValue*100}%`);
         } else {
             let settings = controller.getSettings(msg.guild.id);
             msg.channel.send(`The volume is currently set to ${settings.volume*100}%`);
