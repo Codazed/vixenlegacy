e38c192bd2bc7492d52dbb81371b27b2007e962d
Joshua Luce <joshuabluce@protonmail.com>
2020-05-08 23:55:00 -0500

    Fixed a small issue with the muted command

--- src/commands/moderation/muted.js
+++ src/commands/moderation/muted.js
@@ -19,7 +19,6 @@ module.exports = class MutedCommand extends commando.Command {
     async run(msg, args) {
         let moment = require('moment');
         let muted = botclient.vixen.db.prepare(`select * from muted where guild=?`).all(msg.guild.id);
-
         let {table} = require('table');
         let messageString;
         let data = [['Discord Tag', 'ID', 'Muted On', 'Muted For', 'Muted Until']];
@@ -66,17 +65,21 @@ module.exports = class MutedCommand extends commando.Command {
             }
         }
         let page = 1;
-        muted.forEach(person => {
-            let member = msg.guild.member(person.id);
-            data.push([member.user.tag, person.id, moment.unix(person.muteTimeStart).utc().format("DD/MM/YYYY HH:mm [UTC]"), moment.duration(moment.unix(person.muteTimeEnd).diff(moment.unix(person.muteTimeStart))).humanize(), moment.unix(person.muteTimeEnd).utc().format("DD/MM/YYYY HH:mm [UTC]")]);
-            if (`Muted users page ${page}\`\`\`${table(data, tableCfg)}\`\`\``.length >= 2000) {
-                let overflow = data.pop();
-                msg.channel.send(`Muted users page ${page}\`\`\`${table(data, tableCfg)}\`\`\``);
-                page++;
-                data = [['Discord Tag', 'ID', 'Muted On', 'Muted For', 'Muted Until'], overflow];
-            }
-        });
-        messageString = table(data, tableCfg);
-        await msg.channel.send(`Muted users page ${page}\`\`\`${messageString}\`\`\``);
+        if (muted.length > 0) {
+            muted.forEach(person => {
+                let member = msg.guild.member(person.id);
+                data.push([member.user.tag, person.id, moment.unix(person.muteTimeStart).utc().format("DD/MM/YYYY HH:mm [UTC]"), moment.duration(moment.unix(person.muteTimeEnd).diff(moment.unix(person.muteTimeStart))).humanize(), moment.unix(person.muteTimeEnd).utc().format("DD/MM/YYYY HH:mm [UTC]")]);
+                if (`Muted users page ${page}\`\`\`${table(data, tableCfg)}\`\`\``.length >= 2000) {
+                    let overflow = data.pop();
+                    msg.channel.send(`Muted users page ${page}\`\`\`${table(data, tableCfg)}\`\`\``);
+                    page++;
+                    data = [['Discord Tag', 'ID', 'Muted On', 'Muted For', 'Muted Until'], overflow];
+                }
+            });
+            messageString = table(data, tableCfg);
+            await msg.channel.send(`Muted users page ${page}\`\`\`${messageString}\`\`\``);
+        } else {
+            await msg.channel.send('There are no muted users on this server.');
+        }
     }
 };
